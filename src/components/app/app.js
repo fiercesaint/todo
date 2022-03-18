0a824e381046ce5d56067c083771fc1798af2d5e
fiercesaint <fiercesaint@gmail.com>
2022-03-18 21:05:58 +0300

    css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,13 +11,14 @@ const App = () => {
 
   const todoData = [
     { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
+    { label: 'Learn React', important: true, id: 2 },
+    { label: 'Have some fun', important: false, id: 3 }
   ];
 
   return (
     <div className="todo-app">
       <AppHeader toDo={1} done={3} />
+      
       <div className="top-panel d-flex">
         <SearchPanel />
         <ItemStatusFilter />
